60e448bbe8185293b20d0aa89888477b1eacaafa
phalanx10 <phalanx.1379@gmail.com>
2021-01-07 12:20:11 +0900

    deviseビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   root to: "tasks#index"
   resources :users, only: [:edit, :update]
   resources :tasks
