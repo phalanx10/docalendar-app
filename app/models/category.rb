c86d21a35d862443796a8eb982678d4af993a867
phalanx10 <phalanx.1379@gmail.com>
2021-01-11 13:31:51 +0900

    カテゴリー変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,12 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '仕事' },
-    { id: 3, name: 'プライベート' },
-    { id: 4, name: '趣味' },
+    { id: 2, name: '定例会' },
+    { id: 3, name: '貸切' },
+    { id: 4, name: 'トレーニング' },
     { id: 5, name: '買い物' },
-    { id: 6, name: 'その他' }
+    { id: 6, name: 'カスタム品受取日' },
+    { id: 7, name: 'その他' }
   ]
 
   include ActiveHash::Associations
